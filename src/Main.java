b48c2fa0cad310fe231d3a325e0b3e4358fed1f8
takumi <takumi_1230@outlook.com>
2018-12-20 16:44:31 +0900

    opp

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //難易度入力
         set_Difficulty.BordDifficulty D = set_Difficulty.input(out.start());
-        //ボード生成
+        coordinate XY = new coordinate();
         Square[][] card = new Square[D.Row][D.Column];
-        generate.generate_Card(card, D.NumOfMine);
+        //ボード生成
+        do{
+            generate.generate_Card(card, D.NumOfMine);
+        }while(card[XY.Row][XY.Column].getNumOfMine() == -1);
         out.Game_Over(card);
     }
 }
--- src/input.java
+++ src/input.java
@@ -1,22 +1,14 @@
 import java.util.Scanner;
-public class input {
-    static class coordinate{
-        int Row;
-        int Column;
-        private coordinate (){
-            Row = 0;
-            Column = 0;
-        }
-    }
-    public static coordinate coordinate(){
+class coordinate{
+    int Row = 0;
+    int Column = 0;
+
+    public coordinate(){
         Scanner sc = new Scanner(System.in);
         System.out.println("座標を入力してください( 例;(x,y) → \"x y\" )");
         String str = sc.nextLine();
         String[] S = str.split(" ");
-        coordinate C = new coordinate();
-        C.Row = Integer.parseInt(S[0]);
-        C.Column = Integer.parseInt(S[1]);
-        return C;
+        this.Row = Integer.parseInt(S[0]) - 1;
+        this.Column = Integer.parseInt(S[1]) - 1;
     }
-
 }
